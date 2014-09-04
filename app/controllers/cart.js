11c9adba194e4ac6fbc3d73226cec0cd4b10ca7e
Adam Barnhard <admbarn@gmail.com>
2014-09-04 13:40:00 -0500

    added stripe

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var Product = require('../models/product'),
-    _       = require('lodash');
+    _       = require('lodash'),
+    config  = require('../../config');
 
 exports.add = function(req, res){
   Product.findById(req.body.productId, function(err, product){
@@ -22,12 +23,40 @@ exports.add = function(req, res){
 };
 
 exports.index = function(req, res){
-  res.render('cart/index', {cart:req.session.cart || [], _:_});
+  var total = 0;
+  (req.session.cart || []).forEach(function(p){
+    total += p.price * p.count;
+  });
+  var tax = total * 0.075;
+  total = total + tax;
+
+  req.session.totalCents = Math.round(total * 100);
+  req.session.save(function(){
+    res.render('cart/index', {cart:req.session.cart || [], key:config.stripe.apiKey, total:total, tax:tax});
+  });
 };
 
 exports.destroy = function(req, res){
   req.session.cart = [];
   req.session.save(function(){
     res.redirect('/cart');
+  });
+};
+
+exports.purchase = function(req, res){
+  console.log(req.body);
+
+  var stripe = require('stripe')(config.stripe.apiSecret),
+      stripeToken = req.body.stripeToken;
+  stripe.charges.create({
+    amount: req.session.totalCents,
+    currency: 'usd',
+    card: stripeToken,
+    description: req.user.email || 'anonymous'
+  }, function(err, charge){
+    req.session.cart = [];
+    req.session.save(function(){
+      res.redirect('/profile');
+    });
   });
 };
